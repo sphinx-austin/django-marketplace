010a102c99ec4f343ab89bba809659a39e871e67
sphinx-austin <austinjuniour4@gmail.com>
2023-06-21 22:21:56 +0300

    updates

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u%w#^4=wiu7*6fpo*l-!x#gm3j&0_o4&p^h7^(jt0das9zdc23
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
