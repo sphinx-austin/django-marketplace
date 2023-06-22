8d73e62064a96b973a8a3e2dda94d00e34620e9f
sphinx-austin <austinjuniour4@gmail.com>
2023-06-22 17:25:47 +0300

    add media dir

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# add media file dir(collect image from DB)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
